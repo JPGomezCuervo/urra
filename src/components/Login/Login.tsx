63f8482de167c6148a860805f91e90f93f6f5aed
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-08-17 17:43:10 +0900

    implement basic html structure of Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,22 +1,33 @@
-import style from "./Login.module.css"
+import style from "./Login.module.css";
 
 function Login (){
   return(
-  <div className={style.Container}> 
-      <div className="Frame">
-        <div className="InnerFrame">
-          <h2>Iniciar sesión</h2>
-          <label htmlFor="email">Correo electrónico</label>
-          <input name="email"></input>
-          <label htmlFor="password">Contraseña</label>
-          <input name="password"></input>
-          <p>¿Olvidaste tu contraseña?</p>
-          
-        </div>
-      </div>
-      
+    <div className={style.login_mainContainer}> 
+      <div className={`Frame`}>
 
-  </div>
+        <div className={ `InnerFrame ${style.login_innerframe}` }>
+
+          <h2>Iniciar sesión</h2>
+
+          <div className={style.login_emailContainer}>
+            <label htmlFor="email">Correo electrónico</label>
+            <input name="email"></input>
+          </div>
+
+          <div className={style.login_emailContainer}>
+            <label htmlFor="password">Contraseña</label>
+            <input name="password"></input>
+          </div>
+
+          <p>¿Olvidaste tu contraseña?</p>
+          <button className={`Main_button ${style.login_button_submit}`}>Ingresar</button>
+
+        </div>
+
+      </div>
+
+
+    </div>
   )
 } 
 
